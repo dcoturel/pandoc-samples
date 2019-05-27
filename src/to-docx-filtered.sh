ca09608c5fda1ca7543a716560ca2566ce25966d
Daniel Coturel - Lenovo Ideapad S120 <dcoturel@gmai.com>
2019-05-26 23:37:27 -0300

    Script creation

---
+++ src/to-docx-filtered.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#
+# Create a docx file with --toc from the contents of every file in a directory that contains
+# a given string
+#
+# Motivation:
+# Compile a single file with all approved or requesting for approval files
+#
+# Requires:
+#   1) A folder with n *.md file
+#   2) Pandoc installed
+#
+# Ensures
+#   1) A *.docx files with a TOC at the beggining and a table with changes history at the end
+
+find . -name "*.md" -type f -print0 | xargs -0 grep -l "status: elaborada" | xargs -t pandoc -o elaboradas.docx -s --toc
